5e96649195fa864e6f2c40f80106f968543484e7
Martin Škopek <kiblik1@gmail.com>
2023-08-14 18:12:52 +0200

    add simple test case to validate mock output

--- library/library/apps/web/tests.py
+++ library/library/apps/web/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SanityCheck(TestCase):
+    _TEST_STRING :str = b"Let there be light"
+    def testCorrectInitialText(self) -> None:
+        response = self.client.get('http://localhost:8080/')
+        self.assertEqual(200,response.status_code)
+        self.assertEqual(response.content, SanityCheck._TEST_STRING)
